88201c9b107004f554cb6f812d9233f4f400f776
Aleksndar Kenderesi <alek.kenderesi@gmail.com>
2020-09-10 15:22:01 +0200

    Updates

---
+++ precache-manifest.abb6d4402217abae52284b3bbdf7512f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53a913268c3408e0a9ad73a479f0abb",
+    "url": "/Lorem-Picsum/index.html"
+  },
+  {
+    "revision": "7553b8aa7b53cd440dca",
+    "url": "/Lorem-Picsum/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "1bed6901adc1fb7e5248",
+    "url": "/Lorem-Picsum/static/css/main.834f4ecf.chunk.css"
+  },
+  {
+    "revision": "7553b8aa7b53cd440dca",
+    "url": "/Lorem-Picsum/static/js/2.065abdef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Lorem-Picsum/static/js/2.065abdef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bed6901adc1fb7e5248",
+    "url": "/Lorem-Picsum/static/js/main.1a314477.chunk.js"
+  },
+  {
+    "revision": "1705336906d4a1daeb6b",
+    "url": "/Lorem-Picsum/static/js/runtime-main.9267ec7d.js"
+  },
+  {
+    "revision": "05aa77b14a3e3c5a79b60b00b34e9652",
+    "url": "/Lorem-Picsum/static/media/BalooTamma2-Regular.05aa77b1.ttf"
+  }
+]);
